2dada42bbb66feb3ebe56ac2f06b0ead0848a49e
arrianne <arrianneoshea@hotmail.co.uk>
2020-01-07 19:26:48 +1100

    adding home page with dummy images and basic styling

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
+import { Image, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 export default class Home extends Component {
- render() {
-  return (
-   <div>this is my Home component</div>
-  );
+
+constructor(props) {
+
+  super(props);
+
+  const allPhotos = [
+   {
+    id: 'randomstringimadeup43454356546',
+    url: 'https://baconmockup.com/300/200'
+   },
+   {
+    id: 'randomstringimadeup43523526534565',
+    url: 'https://baconmockup.com/300/200'
+   },
+   {
+    id: 'randomstringimadeup433245234534',
+    url: 'https://baconmockup.com/300/200'
+   }
+  ]
+  this.state = {
+    allPhotos
+  };
+}
+
+render() {
+  // eslint-disable-next-line
+        // our doppelganger images
+        const allImages = this.state.allPhotos.map(photo => {
+          return (
+            <div key={photo.id}>
+              <div style={{minHeight: '215px'}}>
+                <i className="bottom-icon material-icons main-close">close</i>
+                <Image style={{ width: '100%' }} src={photo.url} responsive />
+              </div>
+            </div>
+          );
+        })
+
+        return (
+          <div>
+
+          // our doppelganger images created just above
+          {allImages}
+
+          // bottom nav bar
+          <Container className="bottom-nav">
+            <Row className="show-grid">
+              <Col xs={4} className="col-bottom">
+                  <Link to="/app/album"><i className="bottom-icon material-icons">collections</i></Link>
+              </Col>
+              <Col xs={4} className="col-bottom">
+                  <i className="bottom-icon material-icons">camera_alt</i>
+              </Col>
+              <Col xs={4} className="col-bottom">
+                <i className="bottom-icon material-icons">assignment_return</i>
+              </Col>
+            </Row>
+          </Container>
+
+          </div>
+        );
  }
 }
